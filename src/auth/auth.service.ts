87cf4cd0635401fe72171c17564cb39bc7185978
pnmrvvtl <pnmrv.vtl@gmail.com>
2023-03-11 17:59:18 +0200

    feat: add auth controoler and service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from '../dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+@Injectable({})
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signUp(dto: AuthDto) {
+    try {
+      const hash = await argon.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error.constructor.name === 'PrismaClientKnownRequestError') {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
+  }
+
+  signIn() {}
+}
